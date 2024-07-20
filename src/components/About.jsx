a83d55f74310b0830f8080dae4614f937a185ac6
Ryan3389 <Ryan.Cuthbert12@gmail.com>
2024-07-20 11:41:16 -0400

    added img to folder

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -19,15 +19,17 @@ function About() {
             <article className="tech-container">
                 <div className="tech-div">
                     <h3>Frontend</h3>
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus, assumenda.</p>
+                    <p>I  specialize in creating responsive and dynamic user interfaces  by using HTML and  CSS. I leverage React to build modular and efficient applications, ensuring a seamless user experience. </p>
                 </div>
                 <div className="tech-div">
                     <h3>Server Side</h3>
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus, assumenda.</p>
+                    <p>I am proficient in creating robust and efficient back-end systems. My expertise includes Node.js for building scalable applications and Express.js for routing and handling server-side logic. I design and implement RESTful APIs, ensuring seamless communication between the front end and back end.</p>
                 </div>
                 <div className="tech-div">
                     <h3>Backend / database</h3>
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus, assumenda.</p>
+                    <p>
+                        I have extensive experience in backend development working with PostgreSQL and MongoDB.  I utilize Sequelize for efficient relational database management with PostgreSQL and Mongoose for streamlined interaction with MongoDB. I design robust database schemas, perform data migrations, and ensure optimal performance and security in data handling.
+                    </p>
                 </div>
             </article>
         </section>
