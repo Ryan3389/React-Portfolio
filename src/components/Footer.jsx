2d992a50056fefcc693abbb0e3d3a76004c2aea8
Ryan3389 <Ryan.Cuthbert12@gmail.com>
2024-07-23 08:51:30 -0400

    added footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+function Footer() {
+    return (
+        <footer>
+            <p>Ryan Cuthbert - Full Stack Developer</p>
+            <div className="footer-right">
+                <a href="https://github.com/Ryan3389">GitHub</a>
+                <a href="https://www.linkedin.com/in/ryan-cuthbert/">LinkedIn</a>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
