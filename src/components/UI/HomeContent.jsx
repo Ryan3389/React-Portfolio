09470e6a786b81788748978c1df9987cb769fc00
Ryan3389 <Ryan.Cuthbert12@gmail.com>
2024-07-23 22:32:11 -0400

    updated bio

--- src/components/UI/HomeContent.jsx
+++ src/components/UI/HomeContent.jsx
@@ -10,7 +10,8 @@ function HomeContent() {
                         <div className="intro-text">
                             <p className="intro-name">Ryan Cuthbert</p>
                             <p className="intro-job-title">Full Stack Developer</p>
-                            <p>I am a full stack developer with extensive knowledge in front end development/design, server and backend development using the latest technologies.</p>
+                            {/* <p>I am a full stack developer with extensive knowledge in front end development/design, server and backend development using the latest technologies.</p> */}
+                            <p>I am a full stack web developer who utilizes a robust tech stack for creating dynamic applications. I utilize React to create a dynamic front end, while creating efficient RESTful APIs on the backend, connecting them together with MongoDB, and PostgreSQL.</p>
                         </div>
                         <div className="home-btn-container">
                             <a href="/RyanCuthbert_resume.pdf" download="RyanCuthbert_resume.pdf">Resume</a>
